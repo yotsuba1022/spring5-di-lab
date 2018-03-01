1c59e93816f2950586f490c120af3faac7c2eee0
Carl <yotsuba1022@gmail.com>
2018-03-01 23:14:58 +0800

    Init commit.

---
+++ src/main/java/idv/carl/di/lab/spring5dilab/Spring5DiLabApplication.java
@@ -0,0 +1,12 @@
+package idv.carl.di.lab.spring5dilab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5DiLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5DiLabApplication.class, args);
+	}
+}
